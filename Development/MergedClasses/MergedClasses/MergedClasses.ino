4ce21a4798e080f92ef1e60900e99c466ad96677
idangrady <63195210+idangrady@users.noreply.github.com>
2022-03-26 01:51:49 +0100

    Distinguish Color per Person

--- Development/MergedClasses/MergedClasses/MergedClasses.ino
+++ Development/MergedClasses/MergedClasses/MergedClasses.ino
@@ -6,9 +6,24 @@
 #define LED_3_PIN 11
 #define LED_4_PIN 12
 
+
 #define BUTTON_PIN1 22
 #define BUTTON_PIN2 24
 #define BUTTON_PIN3 26
+
+#define RGBMondayMorning 2
+#define RGBMondayNoon 3
+#define RGBMondayEvening 4
+
+
+#define RGBTuesdayMorning 47
+#define RGBTuesdayNoon 49
+#define RGBTuesdayEvening 51
+
+
+#define RGBWednsdayMorning 39
+#define RGBWednsdayNoon 41
+#define RGBWednsdayEvening 43
 
 #define BUTTON_PINMondayMorning 52
 #define BUTTON_PINMondayNoon 50
@@ -19,7 +34,10 @@
 #define CheckMatch = 35
 
 #define VALUE_PERSON_1 3
-#define VALUE_PERSON_2 257
+#define VALUE_PERSON_2 1
+
+#define DAYINWEEK 3
+#define NUMBitTotal 24
 
 
 class Led {
@@ -69,7 +87,9 @@ public:
         off();
     }
     void on(int i) {
+        Serial.print("Turn on: Pin: "); Serial.println(i);
         digitalWrite(pin[i], HIGH);
+        //delay(500);
     }
     void turnPinOff(int i){ digitalWrite(pin[i], HIGH); }
     void off() {
@@ -236,7 +256,7 @@ public:
         //Serial.println("Bits: ");
         for (int i = 0; i < 3; ++i) {
             Serial.print(myarray[i]);
-            delay(200);
+            //delay(200);
         }
     }
     void getArray(byte(&myarray_)[3]) {
@@ -251,24 +271,31 @@ public:
         byte num_non = 0;
         byte num_eve = 0;
 
+        int totalAccomulate = -1;
         for (byte i = 0; i < 3; ++i)
         {
             String time = "";
             int time_pin = -1;
             int time_int;
 
+            int accomulate = 0;
+
             switch (i) {
             case 0:
                 time_pin = 0;
                 time = "Morning";
+                accomulate = 0;
+
                 break;
             case 1:
                 time_pin = 1;
                 time = "Noon";
+                accomulate = 256;
                 break;
             case 2:
                 time_pin = 2;
                 time = "Evening";
+                accomulate = 65536;
             }
             byte state = myarray[i];
 
@@ -285,6 +312,7 @@ public:
                         Serial.println(time_Day);
                         time_Day = time_pin + 8;
                         listLight[j + i] = time_Day;
+                        accomulate = accomulate + 0;
                         break;
                     case 1:
                         Serial.print(time_Day);
@@ -292,26 +320,37 @@ public:
                         Serial.println(time);
                         time_Day = time_pin + 11;
                         listLight[j + i] = time_Day;
+                        accomulate = accomulate + 1;
+
                         break;
                     case 2:
                         Serial.print("Wednesday ");
                         Serial.println(time);
+                        accomulate = accomulate + 2;
+
                         break;
                     case 3:
                         Serial.print("Thursday ");
                         Serial.println(time);
+                        accomulate = accomulate + 3;
+
                         break;
                     case 4:
                         Serial.print("Friday ");
                         Serial.println(time);
+                        accomulate = accomulate + 4;
+
                         break;
                     case 5:
                         Serial.print("Saturday ");
                         Serial.println(time);
+                        accomulate = accomulate + 5;
+
                         break;
                     case 6:
                         Serial.print("Sunday ");
                         Serial.println(time);
+                        accomulate = accomulate + 6;
                         break;
                     }
                 }
@@ -319,13 +358,13 @@ public:
                 {
                     //cout << "0";
                 }
+                totalAccomulate = totalAccomulate + accomulate;
             }
         }
 
     };
 
 };
-
 
 
 class Board {
@@ -336,6 +375,7 @@ private:
     Person ppl[5];
     Led leds[9];
     Button Buttons[10];
+    RGBLed RHBLeds[8];
 
     TimeButton DaysDottons[10];
 
@@ -345,13 +385,17 @@ private:
     int numLed;
     int numButtons;
     int numDaysButtons;
+    int numRGVLed;
 
+    unsigned long lastPressed = 0; 
+    unsigned long debounceDelay = 1000;
+
+    bool anyPressed;
 
 public:
     //Person(a);
     Board() {};
-
-    Board(byte pin, Person* p, int maxP, Led* led, int numLed, Button* btns, int numButtons, TimeButton* daysButtons, int numDaysButtons)
+    Board(byte pin, Person* p, int maxP, Led* led, int numLed, Button* btns, int numButtons, TimeButton* daysButtons, int numDaysButtons, RGBLed* rgbLedList, int rgbLedListNum)
     {
         this->pin = pin;
 
@@ -359,7 +403,7 @@ public:
         this->numButtons = numButtons;
         this->numLed = numLed;
         this->numDaysButtons = numDaysButtons;
-
+        this->numRGVLed = rgbLedListNum;
 
 
         for (int i = 0; i < maxP; ++i) {
@@ -376,6 +420,10 @@ public:
         for (int i = 0; i < numDaysButtons; ++i) {
             DaysDottons[i] = daysButtons[i];
         }
+        for (int i = 0; i < rgbLedListNum; ++i) 
+        {
+            RHBLeds[i] = rgbLedList[i];
+        }
 
         turnOffLed();
 
@@ -384,6 +432,10 @@ public:
 
         for (int i = 0; i < numLed; ++i) {
             leds[i].off();
+        }
+        for (int i = 0; i < numRGVLed; ++i) 
+        {
+            RHBLeds[i].off();
         }
         
     }
@@ -398,13 +450,14 @@ public:
     }
 
     void lightOneAtTheTime() {
-        delay(2000);
+        //delay(2000);
         for (int i = 0; i < numLed; ++i) {
             leds[i].on();
             delay(2000);
         }
     }
     int detectTimePressed() {
+        updateLastPress();
         for (int i = 0; i < numDaysButtons; ++i) {
 
             if (DaysDottons[i].isPressed()) {
@@ -417,44 +470,160 @@ public:
                 if (DaysDottons[i].isActivated() == true) {
                     DaysDottons[i].deactivate();
                     return  int(-DaysDottons[i].getButtonVal());
-
                 }
             }
         }
         return 0;
     }
+    int checkoverLab( int personone,int person2, int overlap, int i)
+    {
+        //Serial.println(personone);
+        ////Serial.println(person2);
+        ////Serial.println(overlap);
+        //Serial.println(1 << i);
+        //Serial.println(personone & (1 << i));
+        //delay(1000);
+        if (personone & (1 << i))
+        {   
+            Serial.print("Got personone right ");  Serial.println(1 << i);
+            //delay(1000);
+
+        return 0;
+        }
+        else if (person2 & (1 << i))
+        {
+            Serial.print("Got person2 right "); Serial.println(1 << i);
+            //delay(1000);
+            return 1;
+        }
+        else if (overlap & (1 << i))
+        {
+            Serial.println("Overlap");
+            return 2;
+        }
+
+        return -1;
+    }
+    void displayAvailability(int personone, int person2, int overlap)
+    {   
+        for (int n = 0; n < 10; n++) {
+            //// check which RGB should light based on the bits
+            int result = checkoverLab(personone, person2, overlap, n);
+            Serial.print("Result: ");
+            Serial.println(result);
+            //delay(1000);
+            //Serial.print("result: "); Serial.print(result); Serial.print("n: "); Serial.println(n);
+            if (result >-1) 
+            {
+                Serial.print("Day"); Serial.println(n);
+                if (n <= 7) {
+                    // morening
+                    // index = i
+                    RHBLeds[n].on(result);
+                    Serial.print("Size: "); Serial.println(sizeof(RHBLeds));
+                    }
+
+                else if (n < 16 && n >= 8)
+                {
+                    // index = %idxPButton -> second
+                    RHBLeds[int(n % 8)].on(result);
+                }
+                else
+                {
+                    // index = %idxPButton -> overlap -> third
+                    RHBLeds[int(n % 8)].on(result);
+                }
+            }
+            
+        }
+    }
+    void updateLastPress() {
+        this->lastPressed = millis();
+        this->anyPressed = true;
+    }
+    void checkIntAvailabale(int person, int person2) {
+        updateLastPress();
+        // here should come update
+        // update()
+
+        // note: Person 1 is always the one pressing where preson 2 is the one the is pressed.
+        int outInt;
+        int personOneAvailableWithout;
+        int personTwoAvailableWithout;
+
+        for (int i = 0; i < numPerson; ++i) {
+            if (i == 0) { outInt = ppl[i].getavailability(); }
+            else {
+                outInt = outInt & ppl[i].getavailability();
+            }
+        }   
+
+        // display both -> num  = 2 for each value (the both color)
+        personOneAvailableWithout = ppl[person].getavailability() ^ outInt;
+        personTwoAvailableWithout = ppl[person2].getavailability() ^ outInt;
+
+        Serial.print("outInt: "); Serial.println(outInt);
+        Serial.print("personOneAvailableWithout: "); Serial.println(personOneAvailableWithout);
+        Serial.print("personTwoAvailableWithout: "); Serial.println(personTwoAvailableWithout);
+
+        displayAvailability(personOneAvailableWithout, personTwoAvailableWithout, outInt);
+
+    }
+    void checkLastPress() {
+        Serial.println(millis());  Serial.println(lastPressed); Serial.println(millis() - lastPressed);
+
+        if ((millis() - lastPressed > debounceDelay) ) { //&& (anyPressed)
+            // initlize all the RGBLED
+            turnOffLed();
+            anyPressed = false;
+        }
+    }
 };
 
 
-// initlizer Classes
+// Regular LED
 Led led1(LED_1_PIN);
 Led led2(LED_2_PIN);
 Led led3(LED_3_PIN);
 Led led4(LED_4_PIN);
 
-
+// regular Button
 Button button1(BUTTON_PIN1);
 Button button2(BUTTON_PIN2);
 Button button3(BUTTON_PIN3);
 
+//RGBLed
+byte monday[3] = { RGBMondayMorning ,RGBMondayNoon,  RGBMondayEvening };
+byte tuesday[3] = { RGBTuesdayMorning ,RGBTuesdayNoon,  RGBTuesdayEvening };
+byte wednsday[3] = { RGBWednsdayMorning ,RGBWednsdayNoon,  RGBWednsdayEvening };
+
+RGBLed rgbLedmonday(monday, RGBMondayMorning);
+RGBLed rgbLedtuesday(tuesday, RGBTuesdayMorning);
+RGBLed rgbLedwednsday(wednsday, RGBWednsdayMorning);
+
+
+Button button4(BUTTON_PINMondayMorning);
+
+// Time Button - > each Led would have its own value correponding to value it carries
 TimeButton buttonMondayMorning(BUTTON_PINMondayMorning,1,"Monday Morning");
-TimeButton buttonMondayNoon(BUTTON_PINMondayNoon,256, "Monday Noon");
+//TimeButton buttonMondayNoon(BUTTON_PINMondayNoon,256, "Monday Noon");
 
-
+// Init Perople
 Person person_1(VALUE_PERSON_1);
 Person person_2(VALUE_PERSON_2);
 
-
+// List Per objects -> this will be fed to the Board object
 Button arrauButtons[3] = { button1 ,button2 ,button3 };
 Led arrayLED[4] = { led1, led2, led3, led4 };
-Person ppl[1] = { person_1 };
+Person ppl[2] = { person_1 , person_2 };
+RGBLed RgbLeds[3] = { rgbLedmonday ,rgbLedtuesday ,rgbLedwednsday };
 
-
-TimeButton daysButtons[2] = { buttonMondayMorning ,buttonMondayNoon };
+// time button
+TimeButton daysButtons[1] = { buttonMondayMorning  }; // buttonMondayNoon
 
 byte a = 0;
 
-Board b(a, ppl, 1, arrayLED, 4, arrauButtons, 3, daysButtons,2);
+Board b(a, ppl, 2, arrayLED, 4, arrauButtons, 3, daysButtons,1, RgbLeds,3); // TODO: Transfer it to automatic len
 
 
 void setup() {
@@ -464,49 +633,21 @@ void setup() {
 int i = 0;
 void loop() {
     Serial.println("Loop");
-    
+    //rgbLedtuesday.on(0);
+    //delay(1000);
+    //rgbLedtuesday.on(1); delay(1000);
+    if (button4.isPressed()) {
+        b.checkIntAvailabale(0, 1);
+    }
 
-    int numDetect = b.detectTimePressed();
-    Serial.println(numDetect);
+    //int numDetect = b.detectTimePressed();
+    //Serial.println(numDetect);
+    b.checkLastPress();
+    //delay(200);
+    i = i + 1;
 }
 
     
-    //}
-    //if (i == 0) {
-    //    Serial.println("In i==1");
-    //    b.initilizeTimeButton();
-    //    i = 1;
-    //}
-
-    //if (button1.isPressed()) {
-    //    Serial.println("button1");
-
-    //    led1.on();
-    //    led2.off();
-    //    led3.on();
-    //    led4.off();
-    //    person_1.displayAvailable();
-    //}
-    //else if (button2.isPressed()) {
-    //    Serial.println("button2");
-    //    led1.off();
-    //    led2.off();
-    //    led3.on();
-    //    led4.on();
-    //    person_2.displayAvailable();
-    //}
-    //else if (button3.isPressed()) {
-    //    Serial.println("button3");
-    //    //arrauPPL[2].displayAvailable();
-    //    led1.on();
-    //    led2.off();
-    //    led3.on();
-    //    led4.off();
-    //}
-    //b.turnOffLed();
-    ////b.lightOneAtTheTime();
-    //b.detectTimePressed();
-
 
 
 
